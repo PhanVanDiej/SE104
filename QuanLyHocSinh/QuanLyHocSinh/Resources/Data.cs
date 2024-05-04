d9beae46c6773cd92f89d316a04b9df999a930a0
fralcy <quangthinh2924@gmail.com>
2024-05-04 16:51:56 +0700

    add data provide class and connection string

---
+++ QuanLyHocSinh/QuanLyHocSinh/Resources/Data.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyHocSinh.Resources
+{
+    public class Data
+    {
+        public static string connectionString { get; private set; } = @"Data Source=DESKTOP-EU216N6\SQLEXPRESS;Initial Catalog=STUDENT_MANAGEMENT;Integrated Security=True; TrustServerCertificate=True";
+    }
+}
